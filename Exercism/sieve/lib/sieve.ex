f07e81f6d4d0ef41b4cb98173ed8eaba7632def0
Vadim Zotin <vadzimzotsin@gmail.com>
2026-04-02 16:15:34 +0300

    corrected sieve exercism

--- Exercism/sieve/lib/sieve.ex
+++ Exercism/sieve/lib/sieve.ex
@@ -3,18 +3,11 @@ defmodule Sieve do
   @moduledoc """
   recursive implementation the Sieve of Eratosthenes
   """
-  def main(num) when is_integer(num) and num > 1 do
-    2..num
-    |> Enum.to_list
-    |> sieve
-  end
 
-  defp sieve(list, acc \\ [])
-  defp sieve([], acc), do: acc
-  defp sieve([head | tail], acc) do
-    acc = acc ++ [head]
-    to_remove = Enum.filter(tail, &rem(&1, head) == 0)
-    sieve(tail -- to_remove, acc)
-  end
+  def main(num) when num < 2, do: []
+  def main(num), do: 2..num |> Enum.to_list |> sieve
+
+  defp sieve([]), do: []
+  defp sieve([head | tail]), do: [head | sieve(Enum.filter(tail, &rem(&1, head) != 0))]
 
 end
